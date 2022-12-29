3e39bfd17e64234aa38d3e677c877c351a1bb233
zacintech <zacintech@gmail.com>
2022-12-29 16:26:44 +0530

    Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,130 @@
+/*
+
 let js = "amazing";
 
-console.log(40 + 8 + 23 - 10);
+//console.log(40 + 8 + 23 - 10);
       
 
 console.log("Jonas");
-
+//Variable naming convention
 let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-    
+
+let person = "Jonas";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher"
+
+//console.log(`${person} 
+//${myFirstJob} 
+//${myCurrentJob}`);
+
+//Assignment 
+
+let country = "Sri Lanka";
+//let continent = "Asia";
+let population = "20 Million";
+
+//console.log(`${country}
+//${continent}
+//${population}`);
+
+*/
+// Data Types
+
+
+/*
+let javaScriptIsFun = true;
+
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "yes";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+
+
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
+*/
+
+/*
+const country = "Sri Lanka";
+const continent = "Asia";
+//const population = "20 Million";
+const isIsland = true;
+const language = "English";
+//const isIsland = true;
+
+console.log(country);
+console.log(continent);
+console.log(isIsland);
+console.log(language);
+console.log(typeof isIsland);
+*/
+
+// Baisc Operators
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+//2 ** 3 means = 2 to the power of 3 ( 2 * 2 * 2)
+
+const firstName = "Jonas";
+const lastName = "Brothers"
+console.log(firstName + " " + lastName);
+console.log(typeof lastName);
+
+
+let x = 10 + 15;  //25
+x += 10; // x = x + 10 = 35
+x *= 4; // x = x * 4 = 140
+x++;
+x--;
+x--;
+
+console.log(x);
+
+
+// Comparison Operator
+
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 21);
+
+const isFullAge = ageSarah >= 21;
+console.log(now - 1991 > now - 2018);
+*/
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+console.log(x - y);
